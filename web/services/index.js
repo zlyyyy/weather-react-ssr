c07f9fd490bdcadb554c7481c49604ed2dd18d91
zlyyyy <zlang1996@163.com>
2019-11-21 18:05:18 +0800

    fix API_PREIFX

--- web/services/index.js
+++ web/services/index.js
@@ -2,7 +2,7 @@
 import http from '../utils/http';
 import api from './api';
 
-const API_PREIFX = '';
+const API_PREIFX = 'https://weather.zhaoly.cn';
 
 const parseKey = key => {
   let method = 'get';
